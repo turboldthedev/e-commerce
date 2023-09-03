6821004bb6e2baf7265f76ff6c3a3661249e108d
Geto1-creator <84434642+Geto1-creator@users.noreply.github.com>
2023-09-03 19:11:16 +0800

    implemented pagination

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/Header";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Footer } from "@/components/Footer";
 
 
